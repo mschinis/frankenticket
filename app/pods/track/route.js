36016f440a99e9862b1c09c4c294ae9bc59e9a72
jgthms <mschinis@gmail.com>
2015-09-26 21:10:33 +0100

    feat(track): Adds implementation of tracking, tickets

--- app/pods/track/route.js
+++ app/pods/track/route.js
@@ -1,4 +1,49 @@
 import Ember from 'ember';
+import Event from '../event/model';
 
 export default Ember.Route.extend({
+    model(){
+        let event = Event.create();
+        let tickets = [];
+        return {
+            event,
+            tickets
+        }
+    },
+    actions: {
+        sendAvailabilityRequest(){
+            console.log('event', this.get('currentModel.event'));
+            this.set('currentModel.tickets',[
+                {
+                    "name": "SAT 9PM 2PROV DOUBLE",
+                    "availability": false,
+                    "price": "£20.00"
+                },
+                {
+                    "name": "SUN 9PM 2PROV DOUBLE",
+                    "availability": false,
+                    "price": "£15.00"
+                }
+            ]);
+            // Event.availability().then((res)=>{
+            //     if(res.length === 0 ) this.transitionTo('eekke');
+            //     return;
+            //
+            //     // res is array
+            //     /*
+            //     [{
+            //         "name": "SAT 9PM 2PROV DOUBLE",
+            //         "availability": false,
+            //         "price": 0
+            //     }]
+            //     */
+            //     this.set('currentModel.tickets',res);
+            // });
+        },
+        sendTrackRequest(){
+            Event.request().then((res)=>{
+                // All done. show confirmation
+            });
+        }
+    }
 });
