bf4c20c7e1b9cb4c8cc8cd857be458621d5a31e0
jgthms <mschinis@gmail.com>
2015-09-27 02:38:11 +0100

    feat(braintree-form): Add action on successfull collection of payment details

--- app/pods/components/braintree-form/component.js
+++ app/pods/components/braintree-form/component.js
@@ -4,10 +4,15 @@ export default Ember.Component.extend({
     // Required input parameters
     price: 0,
     clientToken: '',
+    action: null,
 
     didInsertElement(){
         braintree.setup(this.get('clientToken'), "dropin", {
-          container: "payment-form"
+          container: "payment-form",
+          onPaymentMethodReceived: this.paymentMethodReceived.bind(this)
         });
+    },
+    paymentMethodReceived(method){
+        this.sendAction('action',method);
     }
 });
